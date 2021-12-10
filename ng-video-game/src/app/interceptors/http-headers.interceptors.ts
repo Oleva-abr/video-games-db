cf1e673c84e26be5bbdc5966e8b25480433bd4ef
Olena <72007221+Oleva-abr@users.noreply.github.com>
2021-12-10 10:04:41 +0200

    connect with api

--- ng-video-game/src/app/interceptors/http-headers.interceptors.ts
+++ ng-video-game/src/app/interceptors/http-headers.interceptors.ts
@@ -13,14 +13,12 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     req = req.clone({
       setHeaders: {
-        //'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
         'x-rapidapi-key': 'f6f9e25fb5msha97bd4f7848b7b2p1e8c47jsn564452d96b95',
         'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
       },
       setParams: {
         key:'b1bd51d20f7a40c38a4c30517d96b8a3',
-        // key: 'e40e743af2c94b0c916a8aa618fb4473',
-      }
+        }
     });
     return next.handle(req);
   }
